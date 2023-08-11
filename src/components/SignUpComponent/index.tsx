090b2ef719feba5bfe6e75d03426a2de32fcc8d6
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 13:05:39 -0300

    feat: implemented signUpComponent

--- src/components/SignUpComponent/index.tsx
+++ src/components/SignUpComponent/index.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import { Button } from '../Button';
+import { Input } from '../Input';
+import { TitleText } from '../TitleText';
 import * as Styled from './styles';
 
 export const SignUpComponent = () => {
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
   return (
     <Styled.SignUpComponent>
-      <Button>Create</Button>
+      <TitleText>Welcome to CodeLeap network!</TitleText>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          paddingTop: 20,
+          gap: 5,
+        }}
+      >
+        <span>Please enter your username</span>
+        <Input
+          type="text"
+          placeholder="John doe"
+          onChange={(event) => {
+            setIsDisabled(event.target.value.length <= 0);
+          }}
+        />
+      </div>
+      <Button
+        disabled={isDisabled}
+        css={{ alignSelf: 'end', marginTop: 15, fontWeight: 600 }}
+      >
+        ENTER
+      </Button>
     </Styled.SignUpComponent>
   );
 };
--- src/components/SignUpComponent/styles.ts
+++ src/components/SignUpComponent/styles.ts
@@ -1,3 +1,11 @@
 import { styled } from '../../config/stitches.config';
+import { Box } from '../Box';
 
-export const SignUpComponent = styled('div', {});
+export const SignUpComponent = styled(Box, {
+  width: '500px',
+  height: '190px',
+  padding: 20,
+
+  display: 'flex',
+  flexDirection: 'column',
+});

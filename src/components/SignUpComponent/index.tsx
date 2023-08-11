914ffc5130553f86531267221526de49e7f5b943
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 11:02:27 -0300

    feat: create signUpComponent

---
+++ src/components/SignUpComponent/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from '../Button';
+import * as Styled from './styles';
+
+export const SignUpComponent = () => {
+  return (
+    <Styled.SignUpComponent>
+      <Button>Create</Button>
+    </Styled.SignUpComponent>
+  );
+};

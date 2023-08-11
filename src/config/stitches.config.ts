7285219f6e0fb1845325b590bbe0b02d40508e8c
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 11:03:10 -0300

    feat: add backgroundLight in theme

--- src/config/stitches.config.ts
+++ src/config/stitches.config.ts
@@ -4,6 +4,7 @@ export const { styled, globalCss, keyframes, css, theme } = createStitches({
   theme: {
     colors: {
       background: '#FFFFFF',
+      backgroundLight: '#DDDDDD',
       primary: '#7695EC',
       warning: '#FF5151',
       Success: '#47B960',

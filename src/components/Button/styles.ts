6a544580f7cec5ac5cf42e18f94b29161dbe5f1b
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 13:05:12 -0300

    feat: style button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,20 @@
 import { styled } from '../../config/stitches.config';
 
-export const Button = styled('button', {});
+export const Button = styled('button', {
+  width: '111px',
+  height: '32px',
+  borderRadius: '8px',
+
+  border: 'none',
+  outline: 'none',
+  background: '$primary',
+
+  cursor: 'pointer',
+
+  color: '#FFFFFF',
+
+  '&[disabled]': {
+    background: '$grey',
+    cursor: 'default',
+  },
+});

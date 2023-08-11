5b2c16ff12d414ca18af784764973a136b5eae7f
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 13:05:48 -0300

    feat: styled signUp Page

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,9 @@
 import { styled } from '../../config/stitches.config';
 
-export const SignUp = styled('div', {});
+export const SignUp = styled('div', {
+  height: '100vh',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});

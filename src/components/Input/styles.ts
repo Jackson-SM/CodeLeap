04549e98ff9e7a64f3af354af34e9a26a7a55a98
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 13:05:22 -0300

    feat: style input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,3 +1,13 @@
 import { styled } from '../../config/stitches.config';
 
-export const Input = styled('input', {});
+export const Input = styled('input', {
+  border: '1px solid $greyDark',
+  borderRadius: '8px',
+  height: '32px',
+  padding: 10,
+  outline: 'none',
+
+  '&::placeholder': {
+    color: '$grey',
+  },
+});

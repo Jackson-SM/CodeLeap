f8b33685e7087c6e152c8facc9c8185e8349b0da
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-11 10:33:47 -0300

    feat: add route Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import SignUp from './pages/Signup';
+import SignUp from './pages/SignUp';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" index element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
   );

9eb9a66b60d2781963825fb69ec152d9067330e6
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-12 18:40:32 -0300

    feat: add screen loading

---
+++ src/components/LoadingScreen/index.tsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+export const LoadingScreen = () => {
+  return (
+    <Styled.LoadingScreen>
+      <Styled.LoadingLetter css={{ fontFamily: 'Manrope' }}>
+        CodeLeap
+      </Styled.LoadingLetter>
+    </Styled.LoadingScreen>
+  );
+};

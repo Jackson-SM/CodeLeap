27e61cc9847e29c0a3aabd8c9219ad31314e27dd
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-12 18:40:41 -0300

    feat: style screen loading

---
+++ src/components/LoadingScreen/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from '../../config/stitches.config';
+
+export const LoadingScreen = styled('div', {
+  background: '$background',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+export const LoadingLetter = styled('span', {
+  fontSize: '4rem',
+});

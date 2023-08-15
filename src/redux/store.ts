963f01da9eba7b07b4a6fac37588fbf863f1978d
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-14 23:20:14 -0300

    feat: apply middleware thunk and create AppDispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, Dispatch } from 'redux';
+import thunkMiddleware, { ThunkDispatch } from 'redux-thunk';
 import rootReducer from './root-reducer';
+import { PostDispatchActions } from '../actions/postActions/postActionTypes';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = Dispatch<PostDispatchActions> &
+  ThunkDispatch<RootState, null, PostDispatchActions>;
 export default store;

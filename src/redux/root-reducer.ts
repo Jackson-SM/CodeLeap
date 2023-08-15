844271fe99922a08851c72487b3507ba461157d4
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-14 23:19:51 -0300

    feat: add post in rootReducer

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -2,13 +2,17 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/reducer';
 import { UserPayload } from '../actions/userActions/actionsTypes';
+import postReducer from './post/reducer';
+import { PostPayload } from '../actions/postActions/postTypes';
 
 export interface AppState {
   user: UserPayload;
+  post: PostPayload;
 }
 
-const rootReducer = combineReducers({
+const rootReducer = combineReducers<AppState>({
   user: userReducer,
+  post: postReducer,
 });
 
 export default rootReducer;
